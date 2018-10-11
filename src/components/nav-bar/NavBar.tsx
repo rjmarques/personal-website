e358f395dff02f3b40d3f4fef18bd8e63cda899f
Ricardo Marques <ricardomarques377@gmail.com>
2018-10-11 20:17:23 +0100

    nav bar style improvements

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -10,15 +10,24 @@ class NavBar extends React.Component {
       <div className="content">
         <nav className="nav-wrapper">
           <Menu
+            className="Nav-menu"
             onClick={this.handleClick}
             defaultOpenKeys={["sub1"]}
             selectedKeys={["1"]}
             mode="horizontal"
           >
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Bio</Menu.Item>
-            <Menu.Item key="3">Career</Menu.Item>
-            <Menu.Item key="4">Education</Menu.Item>
+            <Menu.Item className="Nav-item" key="1">
+              Home
+            </Menu.Item>
+            <Menu.Item className="Nav-item" key="2">
+              Bio
+            </Menu.Item>
+            <Menu.Item className="Nav-item" key="3">
+              Career
+            </Menu.Item>
+            <Menu.Item className="Nav-item" key="4">
+              Education
+            </Menu.Item>
           </Menu>
         </nav>
       </div>
